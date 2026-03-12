f0811bccd3ee222f5673e34fd401a642f2172221
Dean <deanlt.santos@gmail.com>
2026-03-12 22:18:29 +0800

    add flex layout and padding to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return(
-        <header>
+        <header className="flex justify-between px-5 py-3">
             <h1>DS</h1>
             <nav>
-                <ul>
+                <ul className="flex gap-5">
                     <li><a href="">Home</a></li>
                     <li><a href="">About</a></li>
                     <li><a href="">Portfolio</a></li>
