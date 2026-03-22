aa72f603a1c0cf0340ab9e1cc0f9414b994f90b7
Dean <deanlt.santos@gmail.com>
2026-03-22 21:43:31 +0800

    add initial hero section content

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,5 +1,11 @@
 export default function Hero() {
     return(
-        <section></section>
+        <section>
+            <div>
+                <h1>Hi, I am Dean Louis T. Santos</h1>
+                <p>Web Developer</p>
+            </div>
+            <img src="https://media.licdn.com/dms/image/v2/D5603AQE1d7X_1zWzOw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722985330612?e=1775692800&v=beta&t=1AQDuK4Gm2EPmuIhfv7QuW4lITqmYRRQXEF2mDZYTyg" alt="Dean Louis T. Santos"/>
+        </section>
     );
 }
