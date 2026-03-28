3ce6dc64032d8212af86bacd9f0840b4d1b7f7ee
Dean <deanlt.santos@gmail.com>
2026-03-28 11:07:03 +0800

    feat(button):change default background color

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 }
 export default function Button({ label, backgroundColor, textColor, hoverBackgroundColor, hoverTextColor , onClick}: ButtonProps){
     return(
-        <button onClick={onClick} className={`p-3 rounded-xl ${backgroundColor || 'bg-blue-700'} ${textColor || 'text-white'} shadow-md ${hoverBackgroundColor || "hover:bg-white"} ${hoverTextColor || "hover:text-black"} transition duration-300 cursor-pointer`}>
+        <button onClick={onClick} className={`p-3 rounded-xl ${backgroundColor || 'bg-gray-700'} ${textColor || 'text-white'} shadow-md ${hoverBackgroundColor || "hover:bg-white"} ${hoverTextColor || "hover:text-black"} transition duration-300 cursor-pointer`}>
             {label}
         </button>
     )
