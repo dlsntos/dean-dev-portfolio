9cb488d3c27d696ab742678fcc593824b9201ac8
Dean <deanlt.santos@gmail.com>
2026-03-28 09:05:37 +0800

    add container wrapper inside header and add positioning

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
     return(
-        <header className="fixed flex justify-between w-full px-5 py-7 bg-white shadow-md">
-            <h1>DS</h1>
-            <nav>
-                <ul className="flex gap-5">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="">About</Link></li>
-                    <li><Link href="">Portfolio</Link></li>
-                    <li><Link href="">Contact</Link></li>
-                </ul>
-            </nav>
+        <header className="fixed w-full top-0 left-0 py-7 bg-white shadow-md">
+            <div className="flex justify-between w-full max-w-7xl mx-auto">
+                <h1>DS</h1>
+                <nav>
+                    <ul className="flex gap-5">
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="">About</Link></li>
+                        <li><Link href="">Portfolio</Link></li>
+                        <li><Link href="">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
