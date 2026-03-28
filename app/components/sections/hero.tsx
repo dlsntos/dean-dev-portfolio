c0944d782d70dd1b27c89d037453028c1566eb76
Dean <deanlt.santos@gmail.com>
2026-03-28 10:36:47 +0800

    feat(hero-section): import and add button components for view portfolio and resume

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Button from "../button";
+
 export default function Hero() {
     return(
         <section className="relative">
@@ -5,6 +8,10 @@ export default function Hero() {
                 <div>
                     <h1>Hi, I am Dean Louis T. Santos</h1>
                     <p>Full Stack Web Developer</p>
+                    <div>
+                        <Button label="View Portfolio" onClick={() => console.log("clicked!")} />
+                        <Button label="Resume" onClick={() => console.log("clicked!")} />
+                    </div>
                 </div>
             <img src="https://media.licdn.com/dms/image/v2/D5603AQE1d7X_1zWzOw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722985330612?e=1775692800&v=beta&t=1AQDuK4Gm2EPmuIhfv7QuW4lITqmYRRQXEF2mDZYTyg" alt="Dean Louis T. Santos" className="max-w-md"/>
             </div>
