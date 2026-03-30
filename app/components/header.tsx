7abc05ae8a3138a95b6eee25a48c4cfffd673c5b
Dean <deanlt.santos@gmail.com>
2026-03-30 14:17:23 +0800

    feat(header): implement floating effect

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 
 export default function Header() {
-    return(
-        <header className="fixed w-full top-0 left-0 py-7 bg-white shadow-md z-10">
-            <div className="flex justify-between w-full max-w-7xl mx-auto">
-                <h1>DS</h1>
-                <nav>
-                    <ul className="flex gap-5">
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="">About</Link></li>
-                        <li><Link href="">Portfolio</Link></li>
-                        <li><Link href="">Contact</Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header className="fixed w-full top-5 left-0 z-10">
+      <div className="flex justify-between w-full max-w-7xl p-7 bg-white shadow-md mx-auto">
+        <h1>DS</h1>
+        <nav>
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="">About</Link>
+            </li>
+            <li>
+              <Link href="">Portfolio</Link>
+            </li>
+            <li>
+              <Link href="">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
