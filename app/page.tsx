597cad645e118d92fdaba9abae38cf33bff8d73e
Dean <deanlt.santos@gmail.com>
2026-03-30 20:05:30 +0800

    feat(main page): import and add About section component to main tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "./components/header";
 import Hero from "./components/sections/hero";
+import About from "./components/sections/about";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header/>
       <main className="pt-20">
         <Hero />
+        <About />
       </main>
     </div>
   );
