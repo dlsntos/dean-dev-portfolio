3dd537203b0f765d7712a7cc26c5dd9510d2b4ac
Dean <deanlt.santos@gmail.com>
2026-03-30 20:54:09 +0800

    feat(about-section): add html skill icon and remove extra borders

--- app/components/sections/about.tsx
+++ app/components/sections/about.tsx
@@ -1,17 +1,19 @@
+import SkillIcon from "../skill-icon";
+import HTMLIcon from "../skill-icon-svg/html-icon";
 export default function About() {
   return (
     <section className="relative">
-      <div className="py-10 mt-50 w-full max-w-7xl border mx-auto">
+      <div className="py-10 mt-50 w-full max-w-7xl mx-auto">
         <h2 className="text-center text-5xl">About me</h2>
         <div className="flex justify-between items-start">
-          <div className="border w-full">
+          <div className="w-full">
             <img
               src="https://media.licdn.com/dms/image/v2/D5603AQE1d7X_1zWzOw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722985330612?e=1775692800&v=beta&t=1AQDuK4Gm2EPmuIhfv7QuW4lITqmYRRQXEF2mDZYTyg"
               alt="Dean Louis T. Santos"
               className="mt-20 max-w-md"
             />
           </div>
-          <div className="w-full border">
+          <div className="w-full">
             <p className="mt-20 text-2xl max-w-xl">
               I am a Graduating Computer Science student with a passion for
               programming, especially in web development. I am eager to apply my
@@ -20,9 +22,14 @@ export default function About() {
               developer and continuously learn and enhance my technical
               expertise and proficiency.
             </p>
-            <div className="mt-10 border">
+            <div className="mt-10">
               <h3 className="text-2xl">Skills</h3>
-              <div></div>
+              <div>
+                <SkillIcon
+                  icon={<HTMLIcon width={32} height={32} />}
+                  iconName="HTML"
+                />
+              </div>
             </div>
           </div>
         </div>
