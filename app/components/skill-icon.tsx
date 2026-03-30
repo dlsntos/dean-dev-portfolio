348053fdb1fbffc45e1cfbeda369230a438939e9
Dean <deanlt.santos@gmail.com>
2026-03-30 22:33:58 +0800

    feat(skill-icon component): add hover effect

--- app/components/skill-icon.tsx
+++ app/components/skill-icon.tsx
@@ -5,7 +5,7 @@ interface SkillIconProps {
 
 export default function SkillIcon({ icon, iconName }: SkillIconProps) {
   return (
-    <div className="inline-flex items-center text-sm text-white bg-gray-800 px-5 py-1 rounded-full">
+    <div className="inline-flex items-center text-sm text-white bg-gray-800 px-5 py-1 rounded-full transition duration-300 ease-in-out hover:bg-gray-700 hover:scale-105">
       {icon} {iconName}
     </div>
   );
