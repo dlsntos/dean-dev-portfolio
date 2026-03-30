e1a8c2365c5139a8549292aa00f8d7ef1e25dacb
Dean <deanlt.santos@gmail.com>
2026-03-30 19:46:43 +0800

    feat(about--section):add title heading and two empty divs

--- app/components/sections/about.tsx
+++ app/components/sections/about.tsx
@@ -1,7 +1,11 @@
 export default function About() {
-    return(
-        <section>
-            
-        </section>
-    )
-};
+  return (
+    <section className="relative">
+      <div className="py-10 mt-10 w-full max-w-7xl border mx-auto">
+        <h2 className="text-center text-5xl">About me</h2>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
+}
