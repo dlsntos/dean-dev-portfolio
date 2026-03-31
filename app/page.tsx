21cc7d13973b356d3660bbd0b2f3cf09d8ba453a
Dean <deanlt.santos@gmail.com>
2026-03-31 22:04:00 +0800

    feat(main-page): add and import projects-section component

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import Header from "./components/header";
 import Hero from "./components/sections/hero";
 import About from "./components/sections/about";
+import Projects from "./components/sections/projects";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main className="pt-20">
         <Hero />
         <About />
+        <Projects />
       </main>
     </div>
   );
