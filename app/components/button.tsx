dfa4893d03033e67244acc595a3a219bfb140df5
Dean <deanlt.santos@gmail.com>
2026-04-02 17:50:14 +0800

    feat(button): change button component tag to anchort tag

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 interface ButtonProps {
-    label: string;
-    backgroundColor?: string;
-    textColor?: string;
-    hoverBackgroundColor?: string;
-    hoverTextColor?: string;
-    onClick: () => void;
+  label: string;
+  link: string;
+  backgroundColor?: string;
+  textColor?: string;
+  hoverBackgroundColor?: string;
+  hoverTextColor?: string;
 }
-export default function Button({ label, backgroundColor, textColor, hoverBackgroundColor, hoverTextColor , onClick}: ButtonProps){
-    return(
-        <button onClick={onClick} className={`p-3 rounded-xl ${backgroundColor || 'bg-gray-700'} ${textColor || 'text-white'} shadow-md ${hoverBackgroundColor || "hover:bg-white"} ${hoverTextColor || "hover:text-black"} transition duration-300 cursor-pointer`}>
-            {label}
-        </button>
-    )
-};
+export default function Button({
+  label,
+  link,
+  backgroundColor,
+  textColor,
+  hoverBackgroundColor,
+  hoverTextColor,
+}: ButtonProps) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className={`p-3 rounded-xl ${backgroundColor || "bg-gray-700"} ${textColor || "text-white"} shadow-md ${hoverBackgroundColor || "hover:bg-white"} ${hoverTextColor || "hover:text-black"} transition duration-300 cursor-pointer`}
+    >
+      {label}
+    </a>
+  );
+}
