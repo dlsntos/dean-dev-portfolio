a82afabb8666eb778157461d0a6e5a09bf1d13fe
Dean <deanlt.santos@gmail.com>
2026-04-02 17:53:01 +0800

    feat(projects-section): import and add project card component

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -1,8 +1,15 @@
+import ProjectCard from "../project-card";
+
 export default function Projects() {
   return (
-    <section>
-      <div>
-        <h2>Projects</h2>
+    <section className="relative">
+      <div className="py-10 mt-50 w-full max-w-7xl mx-auto">
+        <h2 className="text-center text-5xl">Projects</h2>
+        <div className="mt-20 grid grid-cols-3 gap-5">
+          <ProjectCard projectTitle="Project 1" projectLink="" />
+          <ProjectCard projectTitle="Project 2" projectLink="" />
+          <ProjectCard projectTitle="Project 3" projectLink="" />
+        </div>
       </div>
     </section>
   );
