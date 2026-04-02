9051baab43ebdd3d970e5d43b31d92e3a6183943
Dean <deanlt.santos@gmail.com>
2026-04-02 18:19:19 +0800

    feat(projects-section): add mobile responsiveness

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -3,9 +3,9 @@ import ProjectCard from "../project-card";
 export default function Projects() {
   return (
     <section id="projects" className="relative scroll-mt-24">
-      <div className="py-10 mt-50 w-full max-w-7xl mx-auto">
+      <div className="py-10 mt-50 w-full md:max-w-7xl mx-auto">
         <h2 className="text-center text-5xl">Projects</h2>
-        <div className="mt-20 grid grid-cols-3 gap-5">
+        <div className="mt-20 grid grid-cols-1 md:grid-cols-3 px-5 md:px-0 gap-5">
           <ProjectCard projectTitle="Project 1" projectLink="" />
           <ProjectCard projectTitle="Project 2" projectLink="" />
           <ProjectCard projectTitle="Project 3" projectLink="" />
