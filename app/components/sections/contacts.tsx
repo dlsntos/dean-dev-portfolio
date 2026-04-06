337834e427f65fc986f4cb51dd541895a7769f8c
Dean <deanlt.santos@gmail.com>
2026-04-06 18:48:57 +0800

    feat(contact-section): create contacts-section component

---
+++ app/components/sections/contacts.tsx
@@ -0,0 +1,14 @@
+export default function Contacts() {
+  return (
+    <section>
+      <h2>Get in touch</h2>
+      <div>
+        <ul>
+          <li>Email:</li>
+          <li>Linkedin:</li>
+          <li>Github:</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
