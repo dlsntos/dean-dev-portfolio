939cba8ad531a89810a6f7114ed6567458975e85
Dean <deanlt.santos@gmail.com>
2026-04-06 20:29:19 +0800

    update project card values

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -1,5 +1,5 @@
 import ProjectCard from "../project-card";
-
+import project1 from "../../assets/bcremit-project.png";
 export default function Projects() {
   return (
     <section id="projects" className="relative scroll-mt-24">
@@ -9,14 +9,20 @@ export default function Projects() {
           <ProjectCard
             projectTitle="BCRemit OJT project"
             projectLink="https://github.com/dlsntos/bcremit-clone-website-project"
+            projectDescription="This project is a Fullstack Replica of the BCRemit webapp using React(Typescript), ASP.Net, and Postgresql."
+            image={project1}
           />
           <ProjectCard
             projectTitle="Smart Target Thesis Project"
             projectLink="https://github.com/dlsntos/smart-target-frontend"
+            projectDescription="A Camera Based advertising system for clothing stores using Faster R-CNN and SVM. The techstack used includes React(Javascript), Python(Flask Framework), and Postgresql."
+            image={""}
           />
           <ProjectCard
             projectTitle="Dali Inventory App (Unofficial)"
             projectLink="https://github.com/GuilianiHatsu1k1314/apsi_inventory_app"
+            projectDescription="A Dali Themed Inventory Management System using React(Typescript), TailwindCSS, and Supabase backend."
+            image={""}
           />
         </div>
       </div>
