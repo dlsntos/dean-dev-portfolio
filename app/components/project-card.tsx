b1b175e699817ddf912ca9d7c8f35529dd0a95a9
Dean <deanlt.santos@gmail.com>
2026-04-06 20:28:25 +0800

    feat(project-card): add prop for project description

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -1,32 +1,34 @@
 import Button from "./button";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
 
 interface ProjectCardProps {
   projectTitle: string;
   projectLink: string;
+  projectDescription: string;
+  image: string | StaticImageData;
 }
 export default function ProjectCard({
   projectTitle,
   projectLink,
+  projectDescription,
+  image,
 }: ProjectCardProps) {
   return (
-    <div className="h-auto min-w-[10dvh] md:max-w-[30dvw] rounded-lg bg-gray-100 drop-shadow-xl drop-shadow-gray-300">
+    <div className="flex flex-col h-auto min-w-[10dvh] md:max-w-[30dvw] rounded-lg bg-gray-100 drop-shadow-xl drop-shadow-gray-300">
       <h3 className="text-center text-3xl pt-5">{projectTitle}</h3>
       <div className="flex justify-center w-full h-52 min-w-10 p-5">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeJQeJyzgAzTEVqXiGe90RGBFhfp_4RcJJMQ&s"
+        <Image
+          src={image}
           alt=""
+          width={50}
+          height={50}
           className="w-full h-full object-cover rounded-sm shadow-md shadow-mauve-200"
         />
       </div>
-      <p className="px-10 text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor
-        ultricies ex, sed varius dolor venenatis non. Phasellus velit neque,
-        facilisis non egestas a, rhoncus ut neque. Proin iaculis elit vitae odio
-        hendrerit, sed dictum velit mattis. Nullam vel justo sed massa efficitur
-        tempor.
-      </p>
+      <p className="px-10 text-justify flex-1">{projectDescription}</p>
       <div className="p-10">
-        <Button link={projectLink} label="View Github" />
+        <Button link={projectLink} label="View Source code" />
       </div>
     </div>
   );
