769124482657e14226b8d314dd5b2f49bbb1b721
Dean <deanlt.santos@gmail.com>
2026-04-06 18:54:03 +0800

    feat(projects-section): add project titles and links

--- app/components/sections/projects.tsx
+++ app/components/sections/projects.tsx
@@ -6,9 +6,18 @@ export default function Projects() {
       <div className="py-10 mt-50 w-full md:max-w-7xl mx-auto">
         <h2 className="text-center text-5xl">Projects</h2>
         <div className="mt-20 grid grid-cols-1 md:grid-cols-3 px-5 md:px-0 gap-5">
-          <ProjectCard projectTitle="Project 1" projectLink="" />
-          <ProjectCard projectTitle="Project 2" projectLink="" />
-          <ProjectCard projectTitle="Project 3" projectLink="" />
+          <ProjectCard
+            projectTitle="BCRemit OJT project"
+            projectLink="https://github.com/dlsntos/bcremit-clone-website-project"
+          />
+          <ProjectCard
+            projectTitle="Smart Target Thesis Project"
+            projectLink="https://github.com/dlsntos/smart-target-frontend"
+          />
+          <ProjectCard
+            projectTitle="Dali Inventory App (Unofficial)"
+            projectLink="https://github.com/GuilianiHatsu1k1314/apsi_inventory_app"
+          />
         </div>
       </div>
     </section>
